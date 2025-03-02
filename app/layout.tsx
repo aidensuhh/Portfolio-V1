c261e30f317fc8e070ba64fe65d4aa2bcc37a8d5
aiden <aiden.suh0000@gmail.com>
2025-03-02 18:23:55 -0500

    Fixed extended width and skills animation duration

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="!scroll-smooth">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50 text-gray-950 relative h-[5000px] pt-28 sm:pt-36`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50 text-gray-950 relative h-[5000px] pt-28 sm:pt-36 overflow-x-hidden`}>
         <div className="bg-[#f7dfdf] absolute top-[-6rem] -z-10 right-[-5rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[500rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#eae7fc] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[20rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-24rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
 
